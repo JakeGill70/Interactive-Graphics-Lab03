28ec341940e739d7a079f20c1fb401137caa680a
Jake Gillenwater <JakeGill70@gmail.com>
2021-02-05 12:17:00 -0500

    Milestone 2

---
+++ World2D.js
@@ -0,0 +1,18 @@
+"use strict";
+class World2D {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.context = this.canvas.getContext("2d");
+        this.objects = new Map();
+
+        this.render = function _render() {
+            // Implementation by Dr. Jeff Roach as notes inside of 
+            // "CSCI41575157-WorldTransforms-Animation" on D2L.
+            // https://elearn.etsu.edu/d2l/le/content/8468953/viewContent/72014379/View
+            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.objects.forEach(element => {
+                element.render(this.context);
+            });
+        }
+    }
+}
